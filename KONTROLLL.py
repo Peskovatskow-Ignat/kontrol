0e408f148f4010a707c39dd8c2e3b3727b9a7a61
Peskovatskov Ignat <113009998+Peskovatskow-Ignat@users.noreply.github.com>
2022-12-16 11:52:49 +0300

    Add files via upload

--- KONTROLLL.py
+++ KONTROLLL.py
@@ -18,26 +18,26 @@ class Talking:
 
     def number_yes(self):
         self.count_y += 1
-        return self.count_y
+        return f"{cat.name} произносит - {list_1[0]}: {self.count_y} раз"
 
     def number_no(self):
         self.count_n += 1
-        return self.count_n
+        return f"{cat.name} произносит - {list_1[0]}: {self.count_n} раз"
 
 
 list_1 = ["moore - moore", "meow - meow"]
 cat = Talking("Пушок")
 print(f"Нашего котика зовут {cat.name} у него сегодня хорошее настроение и он делает следующте вещи:")
 time.sleep(3)
-print(f"{cat.name} произносит - {list_1[0]}: {cat.number_yes()} раз")
+print(cat.number_yes())
 time.sleep(2)
 count = 50
 while True:
     i = random.randint(0, 100)
     if i > count:
-        print(f"{cat.name} произносит - {list_1[0]}: {cat.number_yes()} раз")
+        print(cat.number_yes())
         time.sleep(2)
 
     else:
-        print(f"{cat.name} произносит - {list_1[1]}: {cat.number_no()} раз")
+        print(cat.number_no())
         time.sleep(2)
